763aa6a7f33fa7fd701e029ed535a091919ec2e9
Bu4y <amnes1a@windowslive.com>
2017-06-05 13:06:17 +0700

    change statego moredetailctrl

--- www/js/controller/moreDetailCtrl.js
+++ www/js/controller/moreDetailCtrl.js
@@ -84,7 +84,7 @@ adminApp.controller('MoreDetailCtrl', function ($scope, $rootScope, $stateParams
         AccuralreceiptsService.updateaccuralreceipt(accuralreceiptsId, accuralreceipt)
             .then(function (response) {
                 // alert('success');
-                $state.go('tab.listar');
+                $state.go('app.listar');
             }, function (error) {
                 console.log(error);
                 alert('dont success' + " " + error.data.message);
